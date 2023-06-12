eb6e083122c8711dbf6a29f819f390f897168432
EmmaC <c.emma2112@gmail.com>
2023-06-12 07:15:25 +0000

    create post model

--- blog/models.py
+++ blog/models.py
@@ -2,3 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+class Post(models.Model):
+    title = models.CharField(max_length=400, unique=True)
+    slug = models.SlugField(max_length=400, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="devconnect_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    image = CloudinaryField('image', default='placeholder')
+    category = models.TextField()
+    created_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='devconnect_likes', blank=True)
+
