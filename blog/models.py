fc683a5104ba748abe3d569a01310e447f178703
EmmaC <c.emma2112@gmail.com>
2023-06-12 17:06:15 +0000

    create status tuple

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
 class Post(models.Model):
     title = models.CharField(max_length=400, unique=True)
     slug = models.SlugField(max_length=400, unique=True)
