6753b9c2f99b13342c1b9857d5991180e4ee82bc
EmmaC <c.emma2112@gmail.com>
2023-06-12 19:43:29 +0000

    add list filter & display

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 
 class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status','created_on')
     prepopulated_fields = {'slug':('title',)}
     summernote_fields = ('content')
     search_fields = ['title', 'content']
