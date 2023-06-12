0c4e96b1f1f9286b9a64a8f87f5096b9db25659d
EmmaC <c.emma2112@gmail.com>
2023-06-12 07:16:48 +0000

    update category field to char

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     image = CloudinaryField('image', default='placeholder')
-    category = models.TextField()
+    category = models.CharField(max_length=200)
     created_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     likes = models.ManyToManyField(User, related_name='devconnect_likes', blank=True)
