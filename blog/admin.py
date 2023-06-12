0101991c6ddc4c58f724bc903426b417c176ddd5
EmmaC <c.emma2112@gmail.com>
2023-06-12 19:29:50 +0000

    add prepopulated dict to slug field, based on title field

--- blog/admin.py
+++ blog/admin.py
@@ -5,4 +5,5 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 
 class PostAdmin(SummernoteModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
     summernote_fields = ('content')
