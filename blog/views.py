865d468973452f8ff16ef90f501a76687a3a82f7
EmmaC <c.emma2112@gmail.com>
2023-06-13 18:58:51 +0000

    add missing quotations to template name

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,5 @@ from .models import Post
 class PostListView(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
