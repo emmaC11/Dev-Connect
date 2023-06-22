16bee88f2fbf982c28e16e0d4af895460247bd99
EmmaC <c.emma2112@gmail.com>
2023-06-22 07:50:44 +0000

    add relative imports & declare post content view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 class PostListView(generic.ListView):
@@ -7,3 +7,5 @@ class PostListView(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+class PostContentView(View):
