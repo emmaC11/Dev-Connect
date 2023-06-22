ee38e09f84947ec887f7928d3f8e7fd171ca4914
EmmaC <c.emma2112@gmail.com>
2023-06-22 19:20:10 +0000

    add postcontent view to urls,py

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from .import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='homepage')
+    path('', views.PostListView.as_view(), name='homepage'),
+    # path converter
+    path('<slug:slug>/', views.PostContentView.as_view(), name='post_content'),
 ]
