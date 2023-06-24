7efa94fca9c12d5625f25e683688e70a4dd7c010
EmmaC <c.emma2112@gmail.com>
2023-06-24 14:19:01 +0000

    add path about.html using TemplateView as custom view not created

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from .import views
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='homepage'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     # path converter
     path('<slug:slug>/', views.PostContentView.as_view(), name='post_content'),
 ]
