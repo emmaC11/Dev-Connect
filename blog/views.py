4f49a08b82b71b56f6345553e3c723f49cfc33f3
EmmaC <c.emma2112@gmail.com>
2023-06-26 19:35:58 +0000

    store data from comment form in comment_content var

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,13 @@ from django.views import generic, View
 from .models import Post
 from .forms import UserCommentForm
 
+
 class PostListView(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
 
 class PostContentView(View):
     def get(self, request, slug, *args, **kwargs):
@@ -31,6 +33,7 @@ class PostContentView(View):
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
         liked = False
+        comment_content = UserCommentForm(data=request.POST)
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
 
