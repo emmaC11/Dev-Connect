741b21babd6ec2e74e64324c20f820d4c098f223
EmmaC <c.emma2112@gmail.com>
2023-06-27 07:19:16 +0000

    capture post & conditional if user is valid to like & unlike posts

--- blog/views.py
+++ blog/views.py
@@ -61,6 +61,12 @@ class PostContentView(View):
         },)
 
 
-    class PostLikeView(View):
-        def post(self, request, slug):
-            
+class PostLikeView(View):
+    def post(self, request, slug):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+        
