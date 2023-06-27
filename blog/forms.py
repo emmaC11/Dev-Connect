a5c2c81a1fd4aaeaf7a564c9f368eacc8096906a
EmmaC <c.emma2112@gmail.com>
2023-06-27 18:57:07 +0000

    add 2 blank lines before class definition (E302)

--- blog/forms.py
+++ blog/forms.py
@@ -2,12 +2,16 @@ from .models import Comment, Post
 from django import forms
 from django_summernote.widgets import SummernoteInplaceWidget
 
+
 class UserCommentForm(forms.ModelForm):
+
     class Meta:
         model = Comment
         fields = ('body',)
 
+
 class UserPostForm(forms.ModelForm):
+
     class Meta:
         model = Post
         fields = [
