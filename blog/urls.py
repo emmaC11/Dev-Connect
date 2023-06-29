c18ef3a396d8bda9eb10c63446441bbdda908489
EmmaC <c.emma2112@gmail.com>
2023-06-29 16:16:06 +0000

    remove add post related code & add commenting guidelines to about.html

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', views.PostListView.as_view(), name='homepage'),
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
-    path('addpost/', TemplateView.as_view(template_name='add_post.html'), name='add_post'),
     # path converter
     path('<slug:slug>/', views.PostContentView.as_view(), name='post_content'),
     path('like/<slug:slug>', views.PostLikeView.as_view(), name='post_like'),
