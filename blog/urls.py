59f767c29aaf0a88f61ef800bf7d7d1352f69eab
EmmaC <c.emma2112@gmail.com>
2023-06-29 20:47:12 +0000

    fix line too long in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,34 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='homepage'),
-    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path(
+        'about/',
+        TemplateView.as_view(template_name='about.html'),
+        name='about'),
     # path converter
-    path('<slug:slug>/', views.PostContentView.as_view(), name='post_content'),
-    path('like/<slug:slug>', views.PostLikeView.as_view(), name='post_like'),
-    path('comment/delete/<int:comment_id>/', views.DeleteCommentView.as_view(), name='delete_comment'),
-    path('comment/edit/<int:comment_id>/', views.EditCommentView.as_view(), name='edit_comment'),
-    path('post/delete/<slug:slug>', views.DeletePostView.as_view(), name='delete_post'),
-
+    path(
+        '<slug:slug>/',
+        views.PostContentView.as_view(),
+        name='post_content'
+    ),
+    path(
+        'like/<slug:slug>',
+        views.PostLikeView.as_view(),
+        name='post_like'
+    ),
+    path(
+        'comment/delete/<int:comment_id>/',
+        views.DeleteCommentView.as_view(),
+        name='delete_comment'
+    ),
+    path(
+        'comment/edit/<int:comment_id>/',
+        views.EditCommentView.as_view(),
+        name='edit_comment'
+    ),
+    path(
+        'post/delete/<slug:slug>',
+        views.DeletePostView.as_view(),
+        name='delete_post'
+    ),
 ]
