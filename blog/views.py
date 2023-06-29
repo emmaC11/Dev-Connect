0d3cfb8b002aaa4d1ad8f36b4b72de72e5c6107e
EmmaC <c.emma2112@gmail.com>
2023-06-29 20:42:21 +0000

    increase post displayed to 9 before pagination

--- blog/views.py
+++ blog/views.py
@@ -11,7 +11,7 @@ class PostListView(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
-    paginate_by = 6
+    paginate_by = 9
 
 
 class PostContentView(View):
