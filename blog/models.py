68f6986b3db9b2b5b32185de3c8c617f5896331e
EmmaC <c.emma2112@gmail.com>
2023-06-29 20:54:29 +0000

    fix line too long & whitespace in models.py

--- blog/models.py
+++ blog/models.py
@@ -4,17 +4,26 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
 class Post(models.Model):
     title = models.CharField(max_length=400, unique=True)
     slug = models.SlugField(max_length=400, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="devconnect_posts")
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="devconnect_posts"
+    )
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     image = CloudinaryField('image', default='placeholder')
     category = models.CharField(max_length=200)
     created_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    likes = models.ManyToManyField(User, related_name='devconnect_likes', blank=True)
+    likes = models.ManyToManyField(
+        User,
+        related_name='devconnect_likes',
+        blank=True
+    )
 
     class Meta:
         # order posts/articles via created on field
@@ -26,8 +35,13 @@ class Post(models.Model):
     def like_count(self):
         return self.likes.count()
 
+
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
     fname = models.CharField(max_length=80)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
