a4cbcddb5a099ed6e3c0b88b6d240cf43fd539f9
EmmaC <c.emma2112@gmail.com>
2023-06-29 14:56:46 +0000

    creeate edit comment.html, add get form in post_detail with conditionals, add path to new edit view to be created

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('<slug:slug>/', views.PostContentView.as_view(), name='post_content'),
     path('like/<slug:slug>', views.PostLikeView.as_view(), name='post_like'),
     path('comment/delete/<int:comment_id>/', views.DeleteCommentView.as_view(), name='delete_comment'),
+    path('comment/edit/<int:comment_id>/', views.EditCommentView.as_view(), name='edit_comment'),
 
 ]
