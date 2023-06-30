5e99aa7f727609ebf37843f971e5b3071c043171
EmmaC <c.emma2112@gmail.com>
2023-06-30 07:43:17 +0000

    change to prod config

--- devconnect/settings.py
+++ devconnect/settings.py
@@ -29,11 +29,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Dev Env
+# DEBUG = True
 
-#DEBUG = False
-
-#X_FRAME_OPTIONS = 'SAMEORIGIN'
+# Prod Env
+DEBUG = False
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = [
     'developerconnect.herokuapp.com',
